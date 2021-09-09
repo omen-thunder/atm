2f43a160da7e3e016ff1c1c2b8e9aa68bbaea706
Ricky Yip <rickyipcw@pm.me>
2021-09-09 22:57:40 +1000

    Relocate error message bar

--- frontend/src/components/KingsAtm.vue
+++ frontend/src/components/KingsAtm.vue
@@ -2,14 +2,11 @@
   <div class="kings-atm h-screen w-screen overflow-hidden flex flex-col justify-center p-8 bg-gray-800">
 
     <div id="atm-container" class="flex flex-col h-full overflow-auto border-4 border-gray-700 rounded-2xl">
+
       <div class="flex flex-col justify-center my-4">
         <img class="h-48" src="../assets/crown.svg">
         <h1 class="text-4xl lg:text-6xl font-semibold text-yellow-100">XYZ Bank</h1>
         <h2 class="text-lg lg:text-xl text-gray-300 font-semibold">Your Business is Our Business</h2>
-      </div>
-
-      <div class="text-gray-50 rounded-2xl p-2 md:w-2/3 xl:w-1/2 mx-auto h-12 transition-all" :class="formError? 'bg-red-600': 'bg-transparent'">
-        <span v-text="formError" v-if="formError"></span>
       </div>
 
       <div class="flex-grow flex flex-col items-center mt-12">
@@ -21,6 +18,15 @@
 
           <button class="p-2 m-4 text-2xl rounded-3xl bg-blue-400 active:bg-gray-400 hover:bg-blue-300" @click="login()">Login</button>
         </div>
+      </div>
+
+      <div class="text-gray-50 rounded-2xl my-4 p-2 md:w-2/3 xl:w-1/2 mx-auto transition-all" :class="formError? 'bg-red-600': 'bg-transparent'">
+        <span v-text="formError" v-if="formError"></span>
+      </div>
+
+      <div class="text-gray-500 p-2 flex justify-between">
+        <span>{{ machineNumber? 'ATM# ' + machineNumber : 'Powered by KingsATM'}}</span>
+        <SystemTime />
       </div>
 
     </div>
@@ -50,7 +56,7 @@ export default {
     login: async function(){
       if (this.maintenance){
         this.formError = 'Sorry, our system is currently in maintenance. Please try again later.';
-        this.clearFormError(3000);
+        this.clearFormError(5000);
       }
     },
 
