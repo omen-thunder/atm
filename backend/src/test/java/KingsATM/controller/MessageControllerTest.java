dedbddea94b9427ca6854fd4bfb882092c2ca039
Cameron <cam.mcbroom@gmail.com>
2021-09-13 09:38:08 +1000

    Fixed the testing dependencies that were causing jupiter tests not to run

--- backend/src/test/java/KingsATM/controller/MessageControllerTest.java
+++ backend/src/test/java/KingsATM/controller/MessageControllerTest.java
@@ -1,16 +1,14 @@
 package KingsATM.controller;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import org.junit.runner.RunWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
 public class MessageControllerTest {
 
     @Autowired
