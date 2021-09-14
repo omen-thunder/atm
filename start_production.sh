df2827d4dea531ca73ed11e66ba0cd57d9dd6980
Ricky Yip <cyip9287@uni.sydney.edu.au>
2021-09-14 22:44:24 +1000

    Update config for docker environment

--- start_production.sh
+++ start_production.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 branch=$1
 git fetch --all
-git reset --hard origin/"$branch"
+git reset --hard HEAD
+git checkout $branch || git pull origin $branch
+docker-compose build
 docker-compose up -d
