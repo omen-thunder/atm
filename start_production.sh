4fcf913b97eca5ffa96e1da875ecf1d89c7754e9
Ricky Yip <cyip9287@uni.sydney.edu.au>
2021-09-14 23:13:18 +1000

    Better git error handling

--- start_production.sh
+++ start_production.sh
@@ -2,5 +2,6 @@
 branch=$1
 git fetch --all
 git reset --hard HEAD
-git checkout $branch || git pull origin $branch
+git checkout $branch || echo "Could not checkout to $branch"
+git pull origin $branch || echo "Cloud not pull from origin/$branch"
 docker-compose build && docker-compose up -d
