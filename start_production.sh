bfc0d21715c5f3f3c8c3e2d90d0b1d37216aee8a
Ricky Yip <cyip9287@uni.sydney.edu.au>
2021-09-14 22:49:47 +1000

    Do not run if build failed

--- start_production.sh
+++ start_production.sh
@@ -3,5 +3,4 @@ branch=$1
 git fetch --all
 git reset --hard HEAD
 git checkout $branch || git pull origin $branch
-docker-compose build
-docker-compose up -d
+docker-compose build && docker-compose up -d
