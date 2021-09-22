486af9f9194f86dd7e34e73b7f5d9e7d643b6463
Ricky Yip <cyip9287@uni.sydney.edu.au>
2021-09-22 21:03:00 +1000

    Purge CSS for production only

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-  purge: [
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
     './public/*.html',
     './src/**/*.vue',
-  ],
+    ]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
