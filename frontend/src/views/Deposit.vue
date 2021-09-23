b6cd16846581a2ca586fb80e281c6a758ea798bb
Ricky Yip <cyip9287@uni.sydney.edu.au>
2021-09-23 16:08:24 +1000

    Draft deposit page

---
+++ frontend/src/views/Deposit.vue
@@ -0,0 +1,37 @@
+<template>
+
+  <kings-atm-container>
+
+    <div class="flex flex-col justify-center">
+      <div>
+        [Deposit Form Placeholder]
+      </div>
+      <kings-button :buttonType="'primary'" @click="handleCancel">Cancel</kings-button>
+    </div>
+
+  </kings-atm-container>
+
+</template>
+
+<script>
+import KingsAtmContainer from "../components/Base/KingsAtmContainer.vue";
+import KingsButton from '../components/Base/KingsButton.vue';
+
+export default {
+  name: "Deposit",
+  components: {
+    KingsAtmContainer,
+    KingsButton,
+  },
+
+  methods: {
+    async handleCancel() {
+      this.$router.push('/home');
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
